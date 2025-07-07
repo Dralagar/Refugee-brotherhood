43b3055817efee3b724878f6ab168bd025ea3215
Dralagar George <dralagar@gmail.com>
2025-07-08 00:05:00 +0300

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { AnimatePresence } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import CountUp from 'react-countup';
-import styles from '../styles/About.module.css'; // Adjust the path as needed
+import styles from '../styles/About.module.css';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import Image from 'next/image';
 
 // Define the type for a success story
 type SuccessStory = {
