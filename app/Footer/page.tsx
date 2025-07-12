d2782c9c222b360d26e8eb428bf635861a29ccd7
Dralagar George <dralagar@gmail.com>
2025-07-12 17:06:41 +0300

    update

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -39,14 +39,19 @@ const Footer: React.FC = () => {
             </div>
           </div>
           <div className={styles.icons}>
-            <a href="https://facebook.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><FaFacebookF className={styles.icon} style={{width:24, height:24}} /></a>
-            <a href="https://twitter.com/refugeebrother" target="_blank" rel="noopener noreferrer" aria-label="X">
+            <a href="https://www.facebook.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><FaFacebookF className={styles.icon} style={{width:24, height:24}} /></a>
+            <a href="https://x.com/br_refugee" target="_blank" rel="noopener noreferrer" aria-label="X (Twitter)">
               <span className={styles.icon} style={{width:24, height:24, display:'inline-flex', alignItems:'center', justifyContent:'center'}}>
                 <Image src="/images/X logo.png" alt="X (formerly Twitter) Logo - Refugee Brotherhood" width={24} height={24} style={{objectFit: 'contain', width:24, height:24}} />
               </span>
             </a>
-            <a href="https://instagram.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><FaInstagram className={styles.icon} style={{width:24, height:24}} /></a>
-            <a href="https://youtube.com/@refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><FaYoutube className={styles.icon} style={{width:24, height:24}} /></a>
+            <a href="https://www.instagram.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><FaInstagram className={styles.icon} style={{width:24, height:24}} /></a>
+            <a href="https://www.youtube.com/@refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><FaYoutube className={styles.icon} style={{width:24, height:24}} /></a>
+            <a href="https://ke.linkedin.com/company/refugee-brotherhood" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <span className={styles.icon} style={{width:24, height:24, display:'inline-flex', alignItems:'center', justifyContent:'center'}}>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm13.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.89v1.23h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.59v4.72z"/></svg>
+              </span>
+            </a>
           </div>
         </div>
         <div className={styles.column}>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,12 +21,15 @@ const TopContactBar: React.FC = () => (
       <a href="tel:+254794693898" className={styles.topContactItem}><FaPhone /> +254 794 693898</a>
     </div>
     <div className={styles.topContactRight}>
-      <a href="https://facebook.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><FaFacebookF /></a>
-      <a href="https://twitter.com/refugeebrother" target="_blank" rel="noopener noreferrer" aria-label="X">
+      <a href="https://www.facebook.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><FaFacebookF /></a>
+      <a href="https://x.com/br_refugee" target="_blank" rel="noopener noreferrer" aria-label="X (Twitter)">
         <Image src="/images/X logo.png" alt="X (formerly Twitter) Logo" width={24} height={24} style={{objectFit: 'contain'}} />
       </a>
-      <a href="https://instagram.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><FaInstagram /></a>
-      <a href="https://youtube.com/@refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><FaYoutube /></a>
+      <a href="https://www.instagram.com/refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><FaInstagram /></a>
+      <a href="https://www.youtube.com/@refugeebrotherhood" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><FaYoutube /></a>
+      <a href="https://ke.linkedin.com/company/refugee-brotherhood" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm13.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.89v1.23h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.59v4.72z"/></svg>
+      </a>
     </div>
     <div className={styles.topContactDonate}>
       <Link href="/donate" className={styles.donateButton} aria-label="Donate">
