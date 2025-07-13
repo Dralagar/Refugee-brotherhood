5ee6298f775fcd5a2fd2cae765e5a58d5ef0ceb9
Dralagar George <dralagar@gmail.com>
2025-07-13 04:44:44 +0300

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -50,11 +50,11 @@ const TopContactBar: React.FC = () => (
           <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm13.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.89v1.23h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.59v4.72z"/>
         </svg>
       </a>
-      <Link href="/donate" className={styles.donateButton} aria-label="Donate">
+    </div>
+    <Link href="/donate" className={styles.donateButton} aria-label="Donate">
         <span className={styles.donateIcon}>❤️</span>
         <span className={styles.donateText}>Donate</span>
       </Link>
-    </div>
   </div>
 );
 
@@ -160,8 +160,8 @@ const NavBar: React.FC = () => {
                 <Image
                   src="/images/logo.jpg"
                   alt="Refugee Brotherhood Logo"
-                  width={120}
-                  height={60}
+                  width={90}
+                  height={45}
                   priority
                   className={styles.logoImage}
                   style={{
